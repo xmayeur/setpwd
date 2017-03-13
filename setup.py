e41cc499ccae5a96166d754da86a6b05c317f1a8
Xavier Mayeur <xavier@mayeur.be>
2017-03-14 00:06:08 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='crypto_helpers',
+    version='V1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='X. Mayeur',
+    author_email='xavier@mayeur.be',
+    description='Some helper for simple encryption/decryption of messages using either AES 256 or RSA PKCS#1 OAEP'
+)
