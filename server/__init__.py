f026210c5b0603c72161d5a323ceb9c5339484c0
Xavier Mayeur <xavier@mayeur.be>
2018-03-02 22:58:51 +0100

    Add REST API to call service using flask

--- server/__init__.py
+++ server/__init__.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import connexion
-
 from swagger_server import encoder
 
+application = None
 
 def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'local password vault API'})
-    app.run(port=8080)
+    global application
+    application = connexion.App(__name__, specification_dir='./swagger_server/swagger/')
+    application.app.json_encoder = encoder.JSONEncoder
+    application.add_api('swagger.yaml', arguments={'title': 'local password vault API'})
+    application.run(port=8080)
 
 
 if __name__ == '__main__':
