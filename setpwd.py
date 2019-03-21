25eacf1c2c971d2f810d70fb55680bd7507750e7
Xavier Mayeur <xavier@mayeur.be>
2019-03-21 23:55:15 +0100

    initial

--- setpwd.py
+++ setpwd.py
@@ -51,53 +51,56 @@ class RSAcipher:
 
 def main():
 
-    parser = argparse.ArgumentParser("Manage vault passwords")
+    parser = argparse.ArgumentParser(prog='setpwd', description="Manage vault passwords")
 
-    parser.add_argument("-d", "--dump", required=False,
+    parser.add_argument("-D", "--dumpFile", required=False,
                         help="dump all passwords to file")
-    parser.add_argument("-l", "--load", required=False,
+    parser.add_argument("-L", "--loadFile", required=False,
                         help="load all password from file")
-    parser.add_argument("-k", "--genkeys", required=False,
-                        help="generate a keys pair to file")
-    parser.add_argument("-e", "--encrypt", required=False,
-                        help="encrypt the data using the provided key")
-    parser.add_argument("-id", "--identity", required=False,
-                        help="specify id record in the vault datastore")
+    parser.add_argument("-g", "--genkeys", required=False,
+                        help="generate a key pair to files")
+    parser.add_argument("-k", "--key", required=False,
+                        help="encrypt/decrypt the data using the provided key")
+    parser.add_argument("id", nargs='?', default=None,
+                        help="record identifier in the vault datastore")
+    parser.add_argument("-l", "--local", required=False, action='store_true',
+                        help="use a localhost vault server")
+    parser.add_argument("-d", "--delete", required=False, action='store_true',
+                        help="delete the vault entry corresponding to 'id'")
+    parser.add_argument("-r", "--read", required=False, action='store_true',
+                        help="read the vault entry corresponding to 'id'")
+    parser.add_argument("-n", "--new", required=False, action='store_true',
+                        help="create a new vault entry with the identity='id'")
+    parser.add_argument("-c", "--newPassword", required=False, nargs='?', default=None,
+                        help="change the vault entry password corresponding to 'id'")
     parser.add_argument("-u", "--username", required=False,
                         help="specify a user name")
     parser.add_argument("-p", "--password", required=False,
                         help="specify the user password")
-    parser.add_argument("-L", "--local", required=False, action='store_true',
-                        help="use a localhost vault server")
-    parser.add_argument("-D", "--delete", required=False, action='store_true',
-                        help="delete the vault entry ")
-    parser.add_argument("-r", "--read", required=False, action='store_true',
-                        help="only read a vault entry ")
+    
     args = parser.parse_args()
 
     url = 'http://192.168.0.4:5000/api/ID'
     url2 = 'http://192.168.0.4:5000/api/admin'
+    
     headers = {'Content-type': 'application/json'}
-    rsa = None
     curr_name = ''
-    curr_pwd = ''
     uname = ''
-    pwd = ''
     
     if args.local:
         url = 'http://localhost:8080/api/ID'
         url2 = 'http://localhost:8080/api/admin'
     
-    if args.dump:
+    if args.dumpFile:
         r = requests.get(url=url2, headers=headers)
         print(r.json()['status'])
         data = r.json()['data']
-        with open('id.json', 'w') as f:
+        with open(args.dumpFile, 'w') as f:
             json.dump(json.loads(data), f, indent=4)
         sys.exit(0)
         
-    elif args.load:
-        with open('id.json', 'r') as f:
+    elif args.loadFile:
+        with open(args.loadFile, 'r') as f:
             data = json.load(f)
         r = requests.post(url=url2, data=json.dumps(data), headers=headers)
         if r.json()['status'] == 200:
@@ -111,8 +114,8 @@ def main():
         rsa.create_keyset(args.genkeys)
         new_id = dict()
         new_id['id'] = 'pk_'+ args.genkeys
-        new_id['username'] = args.genkeys
-        new_id['password'] = rsa.pubkey.exportKey().decode()
+        new_id['username'] = rsa.pubkey.exportKey().decode()
+        new_id['password'] = args.genkeys
         r = requests.post(url=url, data=json.dumps(new_id), headers=headers)
         if r.json()['status'] == 200:
             print('Key pair created - public key saved in the vault')
@@ -121,55 +124,82 @@ def main():
         sys.exit(0)
         
     else:
-    
-        if args.identity:
-            uid = args.identity
+        # we need a valid vault id and password in all case below
+        if args.id is not None:
+            uid = args.id
         else:
             uid = input('Enter a IdentityId: ')
             if uid == '' or uid is None:
                 print('Please enter a valid Identity name')
                 sys.exit(-1)
-   
-        if args.username:
-            uname = args.username
-        elif not args.read:
-            uname = input('Enter a new User Name: ')
-
+                
+        # for a new record, also ask for a username
+        if args.new:
+            if args.username:
+                uname = args.username
+            else:
+                uname = input('Enter a User Name: ')
+        
         if args.password:
             pwd = args.password
-        elif not args.read:
-            pwd = getpass.getpass('Enter a new password: ')
-            pwd2 = getpass.getpass('Repeat the new password: ')
-            if pwd != pwd2:
-                print('Password mismatch!')
-                sys.exit(-1)
+        else:
+            pwd = getpass.getpass('Enter your password: ')
 
-        # Retrieve a username / password from given Identity
+        # Retrieve the username / password pair using the given Identity
         headers = {'Content-type': 'application/json'}
         r = requests.get(url=url + '?uid=%s' % uid, headers=headers)
         id = r.json()
-        
+
         if id['status'] == 200:
             curr_name = id['username']
             curr_pwd = id['password']
-            
+    
             # decrypt if needed
-            if args.encrypt:
-                rsa = RSAcipher(certfile='priv_' + args.encrypt + '.pem')
+            if args.key:
+                rsa = RSAcipher(certfile='priv_' + args.key + '.pem')
                 curr_name = rsa.decrypt(curr_name)
                 curr_pwd = rsa.decrypt(curr_pwd)
+    
+            # Check if passwords match or exit
+            if curr_pwd != pwd:
+                print('Password mismatch!')
+                exit(-1)
                 
-            if uname == '' and pwd == '':
-                print('Username:password = ' + curr_name + ':'+ curr_pwd)
-                sys.exit(0)
         else:
+            # No vault entry for that id - ok to create a new one with that identity
             curr_uname = ''
             curr_pwd = ''
+
+        # delete a vault record
+        if args.delete:
+            new_id = dict()
+            new_id['id'] = uid
+            new_id['username'] = uname
+            new_id['password'] = pwd
+            r = requests.delete(url=url, data=json.dumps(new_id), headers=headers)
+            if r.json()['status'] == 200:
+                print('Identity removed!')
+            else:
+                print(r.json())
+            sys.exit(0)
+        
+        # change the password in a record
+        if args.newPassword:
+            if pwd is not None:
+                pwd = args.newPassword
+            else:
+                pwd = getpass.getpass('Enter your password: ')
             
-        # create a new username / password entry
-        if uname != '' and pwd != '' and curr_name == '':
-            if args.encrypt:
-                rsa = RSAcipher(certfile='pub_' + args.encrypt + '.pem')
+            if args.username:
+                uname = args.username
+            else:
+                uname = curr_name
+                if uname == '':
+                    uname = input('Enter your username')
+                
+            # encrypt if required
+            if args.key:
+                rsa = RSAcipher(certfile='pub_' + args.key + '.pem')
                 uname = rsa.encrypt(uname)
                 pwd = rsa.encrypt(pwd)
                 
@@ -177,44 +207,44 @@ def main():
             new_id['id'] = uid
             new_id['username'] = uname
             new_id['password'] = pwd
-            r = requests.post(url=url, data=json.dumps(new_id), headers=headers)
+            
+            r = requests.put(url=url, data=json.dumps(new_id), headers=headers)
             if r.json()['status'] == 200:
-                print('Identity saved!')
+                print('Identity updated!')
             else:
                 print(r.json())
             sys.exit(0)
-            
-        # or update an existing entry
-        else:
-
-            if curr_pwd != pwd:
-                print('Password mismatch!')
-                sys.exit(-1)
+        
+        # create a new record
+        if args.new:
+            if curr_name != '' and curr_pwd != '':
+                print('Cannot create a new record with the same id than an existing one')
                 
-            elif args.delete:
+            # create a new username / password entry
+            if uname != '' and pwd != '' and curr_name == '':
+                if args.key:
+                    rsa = RSAcipher(certfile='pub_' + args.key + '.pem')
+                    uname = rsa.encrypt(uname)
+                    pwd = rsa.encrypt(pwd)
+                    
                 new_id = dict()
                 new_id['id'] = uid
                 new_id['username'] = uname
                 new_id['password'] = pwd
-                r = requests.delete(url=url, data=json.dumps(new_id), headers=headers)
+                r = requests.post(url=url, data=json.dumps(new_id), headers=headers)
                 if r.json()['status'] == 200:
-                    print('Identity removed!')
+                    print('Identity saved!')
                 else:
                     print(r.json())
                 sys.exit(0)
-                
-            else:
-                new_id = dict()
-                new_id['id'] = uid
-                new_id['username'] = uname
-                new_id['password'] = pwd1
-                r = requests.put(url=url, data=json.dumps(new_id), headers=headers)
-                if r.json()['status'] == 200:
-                    print('Identity updated!')
-                else:
-                    print(r.json())
-                sys.exit(0)
-                
+
+        # if vault read is requested, display the result if a correct password is given
+        if curr_name != '':
+            print('Username:password = ' + curr_name + ':' + curr_pwd)
+        else:
+            print('Password mismatch or no such identity in the vault')
+        sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
